4e43aaf94686950260e410c7c22943ea5ed4ddea
GonzaELP <gonzalo.dbrito@gmail.com>
2017-04-15 21:35:27 -0300

    Ejercicio 3, funcionando excepcion 0

---
+++ TP 2017/Ejercicio 3/init32.asm
@@ -0,0 +1,256 @@
+;################################################################################
+;#	Título: Ejemplo basico de inicio desde BIOS									#
+;#																				#
+;#	Versión:		2.0							Fecha: 	22/08/2015				#
+;#	Autor: 			D. Garcia					Tab: 	4						#
+;#	Compilación:	Usar Makefile												#
+;#	Uso: 			-															#
+;#	------------------------------------------------------------------------	#
+;#	Descripción:																#
+;#		Inicializacion de un sistema basico desde BIOS							#
+;#		Genera una BIOS ROM de 64kB para inicializacion y codigo principal		#
+;#	------------------------------------------------------------------------	#
+;#	Revisiones:																	#
+;#		2.0 | 22/08/2015 | D.GARCIA | Inicio desde BIOS. Cambio de VMA			#
+;#		1.0 | 04/03/2013 | D.GARCIA | Inicial									#
+;#	------------------------------------------------------------------------	#
+;#	TODO:																		#
+;#		-																		#
+;################################################################################
+
+;********************************************************************************
+; Definicion de la base y el largo de la pila
+;********************************************************************************
+%define STACK_ADDRESS 0x00140000 
+%define STACK_SIZE 32*1024
+
+;********************************************************************************
+; Simbolos externos y globales
+;********************************************************************************
+GLOBAL 		Entry
+EXTERN		start32
+EXTERN          handler_excep0
+EXTERN          __sys_tables_LMA
+EXTERN          __sys_tables_start
+EXTERN          __sys_tables_end
+EXTERN          __main_LMA
+EXTERN          __main_start
+EXTERN          __main_end
+EXTERN          __mdata_LMA
+EXTERN          __mdata_start
+EXTERN          __mdata_end
+EXTERN          __bss_start
+EXTERN          __bss_end
+
+
+
+;********************************************************************************
+; Seccion de codigo de inicializacion
+;********************************************************************************
+USE16
+SECTION 	.reset_vector					; Reset vector del procesador
+
+Entry:										; Punto de entrada definido en el linker
+	jmp 	dword start						; Punto de entrada de mi BIOS
+	times   16-($-Entry) db 0				; Relleno hasta el final de la ROM
+
+;********************************************************************************
+; Seccion de codigo de inicializacion
+;********************************************************************************
+USE32
+SECTION 	.init
+;--------------------------------------------------------------------------------
+; Punto de entrada
+;--------------------------------------------------------------------------------
+start:									; Punto de entrada
+	INCBIN "init16.bin"					; Binario de 16 bits
+
+        
+    ;Saltar a Init32
+    
+;;CODIGO AGREGADO POR GONZALO
+
+;Movimiento del codigo a los lugares que corresponda! los parametros los pasa el Linker Script
+    
+    mov esi, __sys_tables_LMA
+    mov edi, __sys_tables_start
+    mov ecx, __sys_tables_end
+    sub ecx,__sys_tables_start ;calculo la longitud en memoria
+    rep movsb 
+
+    mov esi, __main_LMA
+    mov edi, __main_start
+    mov ecx, __main_end
+    sub ecx,__main_start ;calculo la longitud en memoria
+    rep movsb 
+    
+    mov esi, __mdata_LMA
+    mov edi, __mdata_start
+    mov ecx, __mdata_end
+    sub ecx,__mdata_start ;calculo la longitud en memoria
+    rep movsb 
+    
+    xor eax,eax ;limpio el registro eax para que quede en cero
+    mov edi, __bss_start
+    mov ecx, __bss_end
+    sub ecx, __bss_start
+    rep stosb ;lleno toda la region de memoria correspondiente a variables no inicializadas con ceros
+
+;Carga de las tablas de sistema
+    lgdt [my_gdtr]
+    lidt [my_idtr]
+    
+;Carga de la IDT
+    call InitIDT
+
+
+;Inicializacion de la pila.
+    mov ax,SEL_DATOS
+    mov ss,ax
+    mov esp,STACK_ADDRESS+STACK_SIZE ;(direccion fisica de la pila + tamaño, ojo no pisar otras secciones!!)
+
+;Inicializacion de los PICs
+    mov bx, 0x2028 ; Base de los pics, similar a un paso de parametros por registro.
+    call InitPIC; llamada a la rutina de inicializacion de los PICS
+
+    sti
+;Salto al main
+    mov eax,start32 ;coloco en eax el offset del start
+    push dword SEL_CODIGO ; Pusheo primero el selector de codigo
+    push eax ;luego el offset
+    retf ;si hago un return far saltare a SEL_CODIGO:start32 que es en el archivo "main"
+
+;;CODIGO AGREGADO POR GONZALO
+    
+    
+    
+;--------------------------------------------------------------------------------
+; Inicializacion de la idt
+;--------------------------------------------------------------------------------
+InitIDT:
+
+;Excepcion 0
+    xchg bx,bx 
+    mov eax, handler_excep0
+    mov bx, SEL_CODIGO
+    
+    mov [IDT32], ax
+    mov [IDT32+2],bx
+    mov byte[IDT32+4],0x00
+    mov byte[IDT32+5],0x8E
+    
+    shr eax, 16 ; debo borrar los primeros 16 bits, asi me quedo con la parte alta
+    
+    mov[IDT32+6],ax
+    
+    ret
+    
+    
+;--------------------------------------------------------------------------------
+; Inicializacion del controlador de interrupciones
+; Corre la base de los tipos de interrupción de ambos PICs 8259A de la PC a los 8 tipos consecutivos a 
+; partir de los valores base que recibe en BH para el PIC Nº1 y BL para el PIC Nº2.
+; A su retorno las Interrupciones de ambos PICs están deshabilitadas.
+;--------------------------------------------------------------------------------
+InitPIC:
+										; Inicialización PIC Nº1
+										; ICW1
+	mov		al, 11h         			; IRQs activas x flanco, cascada, y ICW4
+	out     20h, al  
+										; ICW2
+	mov     al, bh          			; El PIC Nº1 arranca en INT tipo (BH)
+	out     21h, al
+										; ICW3
+	mov     al, 04h         			; PIC1 Master, Slave ingresa Int.x IRQ2
+	out     21h, al
+										; ICW4
+	mov     al, 01h         			; Modo 8086
+	out     21h, al
+										; Antes de inicializar el PIC Nº2, deshabilitamos 
+										; las Interrupciones del PIC1
+	mov     al, 0FFh
+	out     21h, al
+										; Ahora inicializamos el PIC Nº2
+										; ICW1
+	mov     al, 11h        			  	; IRQs activas x flanco,cascada, y ICW4
+	out     0A0h, al  
+										; ICW2
+	mov    	al, bl          			; El PIC Nº2 arranca en INT tipo (BL)
+	out     0A1h, al
+										; ICW3
+	mov     al, 02h         			; PIC2 Slave, ingresa Int x IRQ2
+	out     0A1h, al
+										; ICW4
+	mov     al, 01h         			; Modo 8086
+	out     0A1h, al
+										; Enmascaramos el resto de las Interrupciones 
+										; (las del PIC Nº2)
+	mov     al, 0FFh
+	out     0A1h, al
+	ret
+
+;********************************************************************************
+; Tablas de sistema
+;********************************************************************************
+SECTION		.sys_tables 	progbits
+ALIGN 4
+
+;--------------------------------------------------------------------------------
+; GDT
+;--------------------------------------------------------------------------------
+GDT32:
+
+SEL_NULO equ $-GDT32
+    times 8 db 0
+    
+SEL_CODIGO equ $-GDT32
+    db 0xFF; 
+    db 0xFF
+    db 0x00
+    db 0x00
+    db 0x00
+    db 10011000b;
+    db 11001111b;
+    db 0x00
+    
+SEL_DATOS equ $-GDT32
+    db 0xFF
+    db 0xFF
+    db 0x00
+    db 0x00
+    db 0x00
+    db 10010010b;
+    db 11001111b;
+    db 0x00
+LENGTH_GDT equ $-GDT32
+
+my_gdtr: dw LENGTH_GDT-1
+         dd GDT32
+
+
+;--------------------------------------------------------------------------------
+; IDT
+;--------------------------------------------------------------------------------
+
+IDT32:
+;DESC_EXCEP0 equ $-IDT32
+    dq 0x0
+    ;dd handler_excep0 % 0x10000;
+    ;dw SEL_CODIGO
+    ;db 0x0
+    ;db 0x8E
+    ;dw handler_excep0 / 0x10000 
+LENGTH_IDT equ $-IDT32
+
+my_idtr: dw LENGTH_IDT-1
+         dd IDT32
+
+
+
+
+
+;********************************************************************************
+; 						-  -- --- Fin de archivo --- --  -
+; D. Garcia 																c2013
+;********************************************************************************
+
---
+++ TP 2017/Ejercicio 3/main.asm
@@ -0,0 +1,168 @@
+;################################################################################
+;#	Título: Codigo principal de la aplicacion de 32 bits						#
+;#																				#
+;#	Versión:		1.0							Fecha: 	27/04/2015				#
+;#	Autor: 			D. Garcia					Tab: 	4						#
+;#	Compilación:	Usar Makefile												#
+;#	Uso: 			-															#
+;#	------------------------------------------------------------------------	#
+;#	Descripción:																#
+;#	------------------------------------------------------------------------	#
+;#	Revisiones:																	#
+;#		1.0 | 27/04/2015 | D.GARCIA | Inicial									#
+;#	------------------------------------------------------------------------	#
+;#	TODO:																		#
+;#		-																		#
+;################################################################################
+
+;--------------------------------------------------------------------------------
+; Macros
+;--------------------------------------------------------------------------------
+%define DEFAULT_ATTRIBUTES 0x07
+%define VIDEO_BASE_ADDRESS 0xB8000
+
+
+;--------------------------------------------------------------------------------
+; Simbolos externos
+;--------------------------------------------------------------------------------
+GLOBAL		start32
+GLOBAL          print
+GLOBAL          clrscr
+GLOBAL          handler_excep0
+
+;********************************************************************************
+; Datos
+;********************************************************************************
+SECTION 	.data	
+
+atributos dd DEFAULT_ATTRIBUTES;
+fila dd 0
+columna dd 0
+string_ptr db "Hola mundo"
+db 0x0; fin de cadena
+
+msg_excep0 db "Excepcion 0, division por cero"
+db 0x0
+
+USE32
+;********************************************************************************
+; Codigo principal
+;********************************************************************************
+SECTION  	.main 			progbits
+
+start32:
+
+;;CODIGO AGREGADO POR GONZALO
+
+    mov ebp,esp; apunto ebp a esp
+
+    call clrscr; limpio la pantalla
+    
+    ;pusheo de argumento de la funcion print
+    push dword[atributos]
+    push dword[fila]
+    push dword[columna]
+    push string_ptr 
+
+    call print; imprimo el hola mundo
+
+    pop eax
+    pop eax
+    pop eax
+    pop eax
+    
+    xchg bx,bx 
+    
+    mov ebx, 0
+    mov eax, 10
+    div ebx
+    
+    xchg bx,bx
+
+fin:
+    nop
+    jmp fin
+
+;;CODIGO AGREGADO POR GONZALO
+	
+	
+
+;FUNCIONES AGREGADAS
+SECTION .text
+
+
+;void print(char *string_ptr, char columna, char fila, char color)
+;Recibe:
+; EIP de regreso: EBP+4
+; sting_ptr: EBP+8
+; columna: EBP+12
+; fila: EBP+16
+; color: EBP+20
+print:
+    push ebp; guardo el base pointer original.
+    mov ebp, esp; cargo el stack pointer en el base pointer.
+    
+    ;La pantalla es de 80x25, debo hacer fila*80+columna para obtener la posición.
+    mov eax, 160; cargo en eax la la cantidad de caracteres por fila*2 ya que cada caracter ocupa 2 bytes
+    mov ebx, [EBP+16]; cargo en ebx (EBP+16) en la cual quiero escribir y se lo guarda en edx:eax. (aquí estaría en la columna cero).
+    mul ebx;
+    add eax,[EBP+12]; le sumo la columna.
+    add eax,[EBP+12]; le sumo la columna una segunda vez ya que cada caracter ocupa 2 bytes!!
+    add eax, VIDEO_BASE_ADDRESS; le sumo la base de video. Con lo que me queda la dirección de memoria
+    mov edi, eax ;cargo en el di la direccion inicial de memoria sobre la que debo escribir!. edi tendra la direccion donde debo empezar a escribir
+    mov esi, [EBP+8];cargo en el si la direccion de donde comenzaré a sacar los caracteres. esi tendra la direccion donde tengo el primer caracter del texto
+    
+    
+ciclo_print:
+    mov al,byte[esi]
+    mov byte[edi],al;copio el primer caracter del texto en la primera posicion de memoria
+    mov al,byte[EBP+20]
+    mov byte[edi+1],al;copio el color correspondiente en la segunda
+    add edi,2 ;incremento la posicion del destino en 2
+    add esi,1; incremento la posicion de la fuente en 1
+    cmp byte[esi],0
+    jne ciclo_print
+
+fin_print:
+    pop ebp; recupero el valor de ebp original
+    ret
+    
+
+    
+;void clrscr(void)
+;Recibe: NADA 
+clrscr:
+    mov al, 0; caracter nulo
+    mov ah, DEFAULT_ATTRIBUTES; atributos por defecto.
+    mov edi, VIDEO_BASE_ADDRESS
+    mov cx,25*80; cargo en cx el tamaño de la pantalla en "numero de caracteres"
+    
+ciclo_clear:
+    mov word[edi], ax; copio el caracter en la memoria de video
+    add edi,2; incremento dos posicicones ya que cada caracter ocupa 2!
+    loop ciclo_clear
+
+fin_clear:
+    ret
+    
+
+;--------------------------------------------------------------------------------
+; Handlers de excepciones
+;--------------------------------------------------------------------------------
+handler_excep0:
+    
+    call clrscr
+    
+    push dword[atributos]
+    push dword[fila]
+    push dword[columna]
+    push msg_excep0
+    
+    call print; 
+    hlt;
+
+;********************************************************************************
+; 						-  -- --- Fin de archivo --- --  -
+; D. Garcia 																c2013
+;********************************************************************************
+ 
